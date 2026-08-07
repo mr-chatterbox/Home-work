4fe5f17c214928e9dead68793a42219c348cc5d6
mr-chatterbox <shuzaizaan@gmail.com>
2026-08-07 17:20:12 +0530

    HW

--- hw11.cpp
+++ hw11.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 int main(){
 
-   // for (int i = 0; i<=5; i++){
-   for (int i = 0; i<=5; i--){
+    for (int i = 0; i<=15; i += 2){
         cout << i << " ";
+
+        if (i&1){
+            continue;
+        }
+
         i++;
     }
 }
