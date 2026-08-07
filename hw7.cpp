3c430e2b18f0f15ff3570a4daacb014a49237730
mr-chatterbox <shuzaizaan@gmail.com>
2026-08-07 16:56:10 +0530

    HW

---
+++ hw7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 1;
+    int b = 2;
+
+    //if (a-- > 0 && ++b > 2){
+    if (a-- > 0 || ++b > 2){
+        cout << "Stage1 - Inside If";
+    }
+    else{
+        cout << "stage2 - Inside else";
+    }
+    cout << a << " " << b << endl;
+}
---
+++ hw8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   // int num = 3;
+    //cout << (25 * (++num) );
+
+    int a = 1;
+    int b = a++;
+    int c = ++a;
+    cout << b;
+    cout << c;
+}
